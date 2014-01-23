961c9bf4398e4b23a7416ccd2143ed40fa848d2a
robertzk <technoguyrob@gmail.com>
2014-01-23 12:09:00 -0600

    Update zzz.r

--- R/zzz.r
+++ R/zzz.r
@@ -1,6 +1,6 @@
 .onAttach <- function(...) {
   load_github_packages(.github_packages)
-  if (exists('run')) rm('run')
+  if (exists('run', envir = .GlobalEnv, inherits = FALSE)) rm('run', envir = .GlobalEnv)
   makeActiveBinding('run', function() build_model, .GlobalEnv)
 }
 
