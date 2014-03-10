67def3b2928daacaa1d477a2eb5b09e5b1a9b0bf
Irsal Jasebel <ijas@u.northwestern.edu>
2014-03-10 11:07:49 -0500

    spaces in b/w function params

--- inst/tests/test-triggers.r
+++ inst/tests/test-triggers.r
@@ -1,11 +1,11 @@
 context("triggers functions")
 
 test_that("it checks for the base case", {
-  expect_equal(class(trigger),"function")
+  expect_equal(class(trigger), "function")
 })
 
 test_that("it checks to see if all the right classes are being put through", {
-  expect_equal(class(trigger(trigger)),c('syberiaTrigger', 'trigger','function'))
+  expect_equal(class(trigger(trigger)), c('syberiaTrigger', 'trigger','function'))
 })
 
 test_that("it tests to see if triggers is being inherited", {
@@ -14,5 +14,5 @@ test_that("it tests to see if triggers is being inherited", {
 
 test_that("it checks to see if the record function is working", {
   df <- data.frame(x = 1:10, y = letters[1:10])
-  expect_equal(class(record(df)),c('syberiaTrigger', 'trigger','function'))
+  expect_equal(class(record(df)), c('syberiaTrigger', 'trigger','function'))
 })
