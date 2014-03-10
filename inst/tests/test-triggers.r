67d822acc6ae2ab6e04189f4d0e21ba569886e08
Irsal Jasebel <ijas@u.northwestern.edu>
2014-03-10 08:50:15 -0500

    triggers-test done.

--- inst/tests/test-triggers.r
+++ inst/tests/test-triggers.r
@@ -1,6 +1,19 @@
 context("triggers functions testing")
 require(syberia)
 
-test_that("it inherits the right classes", {
-  #not initialized yet
+test_that("it checks for the base case", {
+  expect_equal(class(trigger),"function")
+})
+
+test_that("it checks to see if all the right classes are being put through", {
+  expect_equal(class(trigger(trigger)),c('syberiaTrigger', 'trigger','function'))
+})
+
+test_that("it tests to see if triggers is being inherited", {
+  expect_true(is.trigger(trigger(trigger)))
+})
+
+test_that("it checks to see if the record function is working", {
+  df <- data.frame(x = 1:10, y = letters[1:10])
+  expect_equal(class(record(df)),c('syberiaTrigger', 'trigger','function'))
 })
