563fd49f69eae4060f52a4eb2e64b12ae19654ae
Robert Krzyzanowski <technoguyrob@gmail.com>
2014-03-11 14:01:52 -0500

    import_adapter complete

--- R/import_stage.r
+++ R/import_stage.r
@@ -36,7 +36,35 @@ import_stage <- function(modelenv, import_options) {
 #' @param modelenv an environment. The current modeling environment.
 #' @param import_options a list. Nested list, one adapter per list entry.
 build_import_stagerunner <- function(modelenv, import_options) {
-  lapply(import_options, function() {
-
+  lapply(import_options, function(single_options) {
+    stage <- function(modelenv, opts) {
+      # Only run if data isn't already loaded
+      if (!'data' %in% ls(modelenv)) {
+        attempt <- tryCatch(fn(modelenv, opts), error = function(e) FALSE)
+      }
+    }
+    environment(stage)$fn <- 
   })
 }
+
+#' Fetch an import adapter.
+#'
+#' @param adapter character. Only supported so far are 's3' and 'file'.
+#'    The default is 'file'.
+#' 
+import_adapter <- function(adapter = 'file', opts) {
+  stopifnot(is.character(adapter))
+  adapter <- tolower(adapter)
+  if (adapter == 's3') {
+    function(modelenv, opts) {
+      require(s3mpi)
+      stopifnot(is.character(opts$file))
+      modelenv$data <- s3read(opts$file)
+    }
+  } else {
+    function(modelenv, opts) {
+      modelenv$data <- read.csv(opts$file, stringsAsFactors = FALSE)
+    }
+  }
+}
+
