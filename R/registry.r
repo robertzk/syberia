bf2c1a7b787021f440a418814b757445ced5b47e
RobertZK <technoguyrob@gmail.com>
2014-03-31 22:19:31 -0500

    finish initial functions for syberia registry

--- R/registry.r
+++ R/registry.r
@@ -1,12 +1,12 @@
 get_registry_key <- function(key, registry_dir) {
-  key <- sanitize_registry_key(key, registry_dir)
+  filename <- sanitize_registry_key(key, registry_dir)
   (readRDS(filename)) # do not use default invisibility
 }
 
 set_registry_key <- function(key, value, registry_dir) {
-  key <- sanitize_registry_key(key, registry_dir, read = FALSE)
-  filename <- file.path(registry_dir, key)
-  saveRDS(value, filename)
+  filename <- sanitize_registry_key(key, registry_dir, read = FALSE)
+  tryCatch(saveRDS(value, filename), error = function(e)
+           stop('Failed to save Syberia registry key "', key, "' because: ", e$message))
   key
 }
 
@@ -17,15 +17,19 @@ sanitize_registry_key <- function(key, registry_dir, read = TRUE) {
   if (read) {
     if (!file.exists(filename <- file.path(registry_dir, key)))
       stop('There is no Syberia registry item with key "', key, '"')
+    else if (file.info(filename)$isdir)
+      stop('There is no Syberia registry item with "', key, '", ',
+           'because this key points to a directory.')
+    filename
   } else {
     if ((dir <- dirname(key)) != '.') {
       tryCatch(dir.create(file.path(registry_dir, dir), recursive = TRUE),
-               warning = function(e) {
+               warning = handler <- function(e) {
                  if (grepl("reason 'Not a directory'", e$message))
                    stop('Cannot create Syberia registry key "', key, '"')
                })
     }
+    file.path(registry_dir, key)
   }
-  key
 }
 
