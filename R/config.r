3968518782ec769ec81dfac5d5ca49fa475b503f
RobertZK <technoguyrob@gmail.com>
2014-04-02 18:18:13 -0500

    update github dependencies

--- R/config.r
+++ R/config.r
@@ -1,9 +1,10 @@
 .github_packages <- list(
   list('productivus', 'robertzk'),
   list('Ramd', 'robertzk'),
+  list('frost', 'robertzk'),
+  list('stagerunner', 'robertzk')
   list('mungebitsTransformations', 'robertzk'),
   list('mungebits', 'robertzk'),
   list('tundra', 'robertzk'),
-  list('stagerunner', 'robertzk')
 )
 
