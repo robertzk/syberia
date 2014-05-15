2e96610555d2255f9600d2b5ce2f491fcae302b1
RobertZK <technoguyrob@gmail.com>
2014-05-15 15:59:36 -0500

    fetch_model_container helper for custom tundra classifiers

--- R/model_stage.r
+++ R/model_stage.r
@@ -8,8 +8,7 @@
 #' @export
 model_stage <- function(model_parameters) {
   stopifnot(length(model_parameters) > 0 && is.character(model_parameters[[1]]))
-  if (!exists(model_fn <- pp('tundra_#{model_parameters[[1]]}')))
-    stop("Missing tundra container for keyword '", model_parameters[[1]], "'")
+  model_fn <- fetch_model_container(model_parameters[[1]])
 
   # Remove the model keyword (e.g., "gbm", "glm", etc.)
   model_parameters[[1]] <- NULL
@@ -42,4 +41,54 @@ model_stage <- function(model_parameters) {
 }
 
 
+#' Fetch a tundra model container.
+#'
+#' The container is fetched either from the tundra package or from
+#' the \code{lib/classifiers} directory in the syberia project.
+#'
+#' If there is no tundra model associated to the keyword (like 'gbm'
+#' or 'regularization'), you must place a function called \code{train}
+#' and a function called \code{predict} in the an R file with the same
+#' name as the keyword for your classification. For example,
+#' if you are implementing least-angle regression, you could define
+#' \code{lib/classifiers/lar.R} and from your syberia model use
+#' \code{'lar'} for your model keyword.
+#'
+#' @param type character. The model keyword. This function will attempt
+#'   to fetch the associated container construction function (with
+#'   parameters \code{munge_procedure}, \code{default_args}, and
+#'   \code{internal} from either (1) the tundra package, or (2)
+#'   your syberia project's \code{lib/classifiers} directory (see
+#'   description).
+#' @return A container construction function (with
+#'   parameters \code{munge_procedure}, \code{default_args}, and
+#'   \code{internal} which takes these and return a \code{tundra_container}
+#'   object with a \code{train} and \code{predict} method.
+#' @export
+fetch_model_container <- function(type) {
+  if (!exists(model_fn <- pp('tundra_#{type}'))) {
+    filename <- file.path(syberia_root(), 'lib', 'classifiers', type)
+    if (!(exists(filename <- pp('#{filename}.r')) ||
+          exists(filename <- pp('#{filename}.R')))) {
+      stop("Missing tundra container for keyword '", type, "'. ",
+           "It must exist in the tundra package or be present in ",
+           pp("lib/classifiers/#{type}.R"), call. = FALSE)
+    }
+    
+    provided_env <- new.env()
+    source(filename, local = provided_env)
+    if (!all(c('train', 'predict') %in% ls(provided_env)) &&
+         is.function(provided_env$train) && is.function(provided_env$predict))
+      stop(pp("The file lib/classifiers/#{type}.R must contain both ",
+              "'train' and 'predict' functions."), call. = FALSE)
+
+    model_fn <- function(munge_procedure = list(), default_args = list(),
+                         internal = list()) {
+      tundra:::tundra_container$new(type, provided_env$train, provided_env$predict,
+                                    munge_procedure, default_args, internal)
+    }
+  }
+
+  model_fn
+}
 
