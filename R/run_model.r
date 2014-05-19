aafb6db517b19df84e1533abffed0b59f0ad50e9
RobertZK <technoguyrob@gmail.com>
2014-05-19 17:18:19 -0500

    fixes for local sourcing

--- R/run_model.r
+++ R/run_model.r
@@ -4,36 +4,18 @@
 #'   file with name \code{model_stages} followed by \code{.r} so that syberia
 #'   can read the model configurations.
 #' @export
-run_model <- function(key = get_cache('last_model') %||%
+run_model <- function(key = syberiaStructure:::get_cache('last_model') %||%
                       getOption('syberia.default_model'),
                       ..., fresh = FALSE, verbose = TRUE) {
   src_file <- NULL
   root <- NULL
 
-  # This is a helper function that will be used to track which files are
-  # loaded by a syberia model. Namely, the "source" function will get overwritten
-  # by this one, which will keep track of whether any of the files have been
-  # modified since last encountered by Syberia.
   syberiaStructure:::set_cache(TRUE, 'runtime/executing')
   syberiaStructure:::set_cache(FALSE, 'runtime/any_modified')
   on.exit(syberiaStructure:::set_cache(FALSE, 'runtime/executing'))
-  #source <- local({
-  #  any_modified <- FALSE
-  #  function(file, ...) {
-  #    provides <- list(source = source)
-  #    if ('Ramd' %in% .packages()) provides$define <-
-  #
-  #    if (mock_define(
-  #    resource <- syberia_resource(file, syberia_root(),
-  #                                 provides = , ...)
-  #    any_modified <<- any_modified || resource$modified
-  #    list(value = resource$value(), invisible = TRUE)
-  #  }
-  #})
 
   # Used by syberiaStructure::syberia_resource
   syberiaStructure:::set_cache(parent.frame(), 'runtime/current_env')
-  source <- syberiaStructure::source
 
   model_stages <- 
     #if (missing(key) && is.stagerunner(tmp <- active_runner())) tmp
@@ -46,39 +28,40 @@ run_model <- function(key = get_cache('last_model') %||%
           stop(pp("No file for model '#{key}'"))
       } else root <- syberia_root(src_file) # Cache syberia root
       message("Loading model: ", src_file)
-      source(file.path(root %||% syberia_root(), 'models', src_file))$value
+      model_filepath <- file.path(root %||% syberia_root(), 'models', src_file)
+      resource <- syberia_resource_with_modification_tracking(model_filepath, root, body = FALSE)
+      resource_dir <- dirname(model_filepath)
+      if (basename(resource_dir) ==
+          substring(tmp <- basename(model_filepath), 1, nchar(tmp) - 2)) {
+        # If the model is in its own subdirectory, check all helpers files for
+        # modification time as well.
+        helper_files <- list.files(resource_dir, recursive = TRUE)
+        # Trigger syberia_resource_with_modification_tracking to update whether
+        # or not any helper files were modified.
+        for (file in helper_files) syberia_resource_with_modification_tracking(
+          file.path(resource_dir, file), root, body = FALSE)
+      }
+      resource$value()
     }
     else if (is.list(key)) key
     else if (is.stagerunner(key)) key
     else stop("Invalid model key")
   
   if (is.null(src_file))
-    src_file <- get_cache('last_model')
+    src_file <- syberiaStructure:::get_cache('last_model')
   if (is.null(root)) root <- syberia_root(src_file)
 
   display_file <- src_file
   src_file <- file.path(root, 'models', src_file)
 
-  # Coalesce the stagerunner if model file updated
-  coalesce_stagerunner <- FALSE
-  if (missing(key) && is.character(key) &&
-      is.character(tmp <- get_cache('last_model')) && key == tmp) {
-    if (!is.null(old_timestamp <- get_registry_key(
-        'cached_model_modified_timestamp', get_registry_dir(root)))) {
-      new_timestamp <- file.info(src_file)$mtime
-      if (new_timestamp > old_timestamp) coalesce_stagerunner <- TRUE
-    }
-  }
-
-  set_cache(display_file, 'last_model')
-  set_registry_key('cached_model_modified_timestamp',
-                   file.info(src_file)$mtime, get_registry_dir(root))
+  syberiaStructure:::set_cache(display_file, 'last_model')
 
   # TODO: Figure out how to integrate tests into this. We need something like:
   tests_file <- file.path(root, 'models', gsub('^[^/]+', 'test', display_file))
   testrunner <- NULL
   if (file.exists(tests_file)) {
-    tests <- source(tests_file)$value
+    resource <- syberia_resource_with_modification_tracking(tests_file, root, body = FALSE)
+    tests <- resource$value()
     testrunner <- stageRunner$new(new.env(), tests)
     testrunner$transform(function(fn) {
       require(testthat)
@@ -87,19 +70,31 @@ run_model <- function(key = get_cache('last_model') %||%
     })
   }
 
-  browser()
+  # Determine whether any of the files related to the model have changed
+  # TODO: (RK) Use cleverer overloading of "source" later for this.
+
+  # Coalesce the stagerunner if model file updated
+  coalesce_stagerunner <- 
+    (#missing(key) && is.character(key) && # TODO: (RK) Figure out if this is necessary
+      is.character(tmp <- syberiaStructure:::get_cache('last_model')) &&
+      identical(display_file, tmp) &&
+      syberiaStructure:::get_cache('runtime/any_modified')) 
+
   if (coalesce_stagerunner) {
     stagerunner <- construct_stage_runner(model_stages)
-    stagerunner$coalesce(get_cache('last_stagerunner'))
-  } else if (!missing(key) || !is.stagerunner(stagerunner <- get_cache('last_stagerunner'))) {
+    stagerunner$coalesce(syberiaStructure:::get_cache('last_stagerunner'))
+  } else if (!missing(key) || !is.stagerunner(
+      stagerunner <- syberiaStructure:::get_cache('last_stagerunner'))) {
     stagerunner <- construct_stage_runner(model_stages)
   }
+  # TODO: (RK) Figure out a better wrapping mechanism for this
   if (!is.null(testrunner)) stagerunner$overlay(testrunner, 'tests')
 
   message("Running model: ", display_file)
   out <- tryCatch(stagerunner$run(..., verbose = verbose),
            error = function(e) e)
-  set_cache(stagerunner, 'last_stagerunner')
+  # TODO: (RK) Attempt to record stack trace using evaluate:::try_capture_stack
+  syberiaStructure:::set_cache(stagerunner, 'last_stagerunner')
 
   if (inherits(out, 'simpleError'))
     stop(out$message)
