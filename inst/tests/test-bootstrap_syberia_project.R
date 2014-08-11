bd89b94e7dde8271c2b395cba7069f5294c7541b
RobertZK <technoguyrob@gmail.com>
2014-08-11 12:10:17 -0500

    test bootstrapping

--- inst/tests/test-bootstrap_syberia_project.R
+++ inst/tests/test-bootstrap_syberia_project.R
@@ -1,5 +1,24 @@
 context('bootstrap_syberia_project')
 library(testthatsomemore)
 
-# TODO: (RK) Make this.
+test_that('it can bootstrap a trivial syberia project correctly', {
+  within_file_structure(list(config = list('application.R')), {
+    assert(syberia_project(tempdir))
+  })
+})
 
+test_that('it can bootstrap routes for a simple syberia project', {
+  within_file_structure(list('foo.R', config = list('application.R', 'routes.R' = 
+    deparse(quote(list(foo = function() 'test'))))), {
+    d <- syberia_project(tempdir)
+    expect_identical(d$resource('foo')$value(), 'test')
+  })
+})
+
+test_that('it can define a controller through routes', {
+  within_file_structure(list('foo.R', config = list('application.R', 'routes.R' = 
+    "list(foo = 'test')"), lib = list(controllers = list(test.R = 'function() "test"'))), {
+    d <- syberia_project(tempdir)
+    expect_identical(d$resource('foo')$value(), 'test')
+  })
+})
