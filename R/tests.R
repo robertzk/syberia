249c4945a1e68d6d994a7a786d7eeb3b65d31e1c
RobertZK <technoguyrob@gmail.com>
2014-10-21 18:13:38 -0500

    by default use syberia_project() for tests

--- R/tests.R
+++ R/tests.R
@@ -28,7 +28,7 @@
 #' @return \code{TRUE} if all tests pass or will error otherwise. Note this
 #'    function uses \code{pblapply} from the \code{pbapply} package to
 #'    represent progress.
-test_project <- function(project, base = '') {
+test_project <- function(project = syberia_project(), base = '') {
   if (is.character(project)) project <- syberia_project(project)
   test_path <- file.path(project$root(), 'test')
   tests <- project$find(base = paste0('test/', base))
