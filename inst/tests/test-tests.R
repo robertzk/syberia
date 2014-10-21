e76ce11ced1639f3a66b2fa6e7f44e98f88c5a08
RobertZK <technoguyrob@gmail.com>
2014-10-21 13:50:41 -0500

    provide access to project director in test environment

--- inst/tests/test-tests.R
+++ inst/tests/test-tests.R
@@ -16,6 +16,14 @@ test_that('it can execute a simple test correctly', {
   })
 })
 
+test_that('it can give the test environment access to the director', {
+  within_file_structure(list(config = list('application.R',
+      environments = list(test.R = 'd <- class(director)'))), {
+    p <- syberia_project(tempdir)
+    expect_equal(as.character(p$resource('config/environments/test')$value()$d), 'director')
+  })
+})
+
 # TODO: (RK) Test `base` parameter.
 
 context <- old_context
