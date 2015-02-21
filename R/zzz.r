f05d231a038b8acce00c2778f18728b9f635bad1
RobertZK <technoguyrob@gmail.com>
2015-02-21 13:03:30 -0600

    deprecate auto_run

--- R/zzz.r
+++ R/zzz.r
@@ -1,16 +1,6 @@
 .onAttach <- function(...) {
-  if (!identical(silent <- getOption("syberia.silent"), TRUE)) {
-    message(paste0("Loading ", director:::colourise('Syberia', 'red'), "...\n"))
-  }
-
-  #if (exists('run', envir = .GlobalEnv, inherits = FALSE)) {
-  #  rm('run', envir = .GlobalEnv)
-  #}
-
-  #makeActiveBinding('run', function() run_model, .GlobalEnv)
-
-  if (!isTRUE(getOption('syberia.do_not_autorun'))) {
-    addTaskCallback(auto_run)
+  if (!isTRUE(getOption("syberia.silent"))) {
+    packageStartupMessage(paste0("Loading ", director:::colourise('Syberia', 'red'), "...\n"))
   }
 }
 
