98e8367e72ff30fb98e1bd721b888bb155c03578
RobertZK <technoguyrob@gmail.com>
2015-02-21 12:33:44 -0600

    remove obsolete trigger utilities

--- R/utils.r
+++ R/utils.r
@@ -3,36 +3,15 @@
 
 #' Fetch the current Syberia version.
 #' @export
-syberia_version <- function() utils::packageVersion('syberia')
+syberia_version <- function() { utils::packageVersion('syberia') }
 
 package_exists <- function(name) {
   is.element(name, base::.packages())
 }
 
 as.list.environment <- function(env) {
-    out <- base::as.list.environment(env)
+  out <- base::as.list.environment(env)
   lapply(out, function(x) if (is.environment(x)) as.list(x) else x)
-}
-
-#' Normalize a file name.
-#'
-#' Given a string, will check if a file by that name exists.
-#' If not, it will try to append '.r' and try again.
-#' If not, it will try to append '.R' and try again.
-#' Otherwise, it returns FALSE.
-#'
-#' @param filename a character. The name of a file.
-#' @examples
-#' \dontrun{
-#' normalize_filename('test') # will find test.r or test.R
-#' normalize_filename('test.r') # just returns test.r
-#' }
-normalized_filename <- function(filename) {
-  # TODO: Extend to regular expressions
-  if (file.exists(filename)) filename
-  else if (file.exists(tmp <- pp("#{filename}.r"))) tmp
-  else if (file.exists(tmp <- pp("#{filename}.R"))) tmp
-  else FALSE
 }
 
 #' Ensure no global variables are polluted during an expression.
