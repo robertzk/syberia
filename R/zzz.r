bb17c05acc582172d9768268b67b147dd33848ed
RobertZK <technoguyrob@gmail.com>
2015-02-21 12:19:07 -0600

    remove legacy run_model function

--- R/zzz.r
+++ R/zzz.r
@@ -3,11 +3,11 @@
     message(paste0("Loading ", director:::colourise('Syberia', 'red'), "...\n"))
   }
 
-  if (exists('run', envir = .GlobalEnv, inherits = FALSE)) {
-    rm('run', envir = .GlobalEnv)
-  }
+  #if (exists('run', envir = .GlobalEnv, inherits = FALSE)) {
+  #  rm('run', envir = .GlobalEnv)
+  #}
 
-  makeActiveBinding('run', function() run_model, .GlobalEnv)
+  #makeActiveBinding('run', function() run_model, .GlobalEnv)
 
   if (!isTRUE(getOption('syberia.do_not_autorun'))) {
     addTaskCallback(auto_run)
