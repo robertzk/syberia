a9746b95009835c39876a613133f8df25aec9e94
Peter Hurford <peter@peterhurford.com>
2015-02-23 13:53:12 -0600

    Strange test

--- R/zzz.r
+++ R/zzz.r
@@ -2,7 +2,7 @@
   if (!isTRUE(getOption("syberia.silent"))) {
     packageStartupMessage(paste0("Loading ", director:::colourise('Syberia', 'red'), "...\n"))
   }
-
+  browser()
   # TODO: (RK) Find syberia project off getwd() and call syberia_project().
 }
 
