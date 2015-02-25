6da90d4c75e67b515ab706d0f1ea6fdfa2d323fe
Kirill Sevastyanenko <kirill.sevastyanenko@avantcredit.com>
2015-02-25 12:36:20 -0600

    dry'er generators

--- R/bootstrap.R
+++ R/bootstrap.R
@@ -20,22 +20,32 @@ syberia_new <- function(name) {
   system("yo syberia")
 }
 
+available_types <- c("model", "classifier", "mungebit")
+
+syberia_subgenerator <- function(name, type) {
+  stopifnot(is.character(type) && length(type) == 1)
+  if (!type %in% available_types)
+    stop("Syberia can currently generate only thos types:", model, sep=" ")
+  if (is.null(syberia_root())) stop("You need to create a syberia project first. Check if you are in the right directory.")
+  name <- ifelse(missing(name), "", name)
+  system(paste0("yo syberia:", type, " ", name))
+}
+
 #' Bootstrap a new model for a syberia project
 #'
 #' @param name character. The name of the model.
 #' @export
-syberia_new_model <- function(name) {
-  stopifnot(is.character(name) && length(name) == 1)
-  if (is.null(syberia_root())) stop("You need to create a syberia project first. Check if you are in the right directory.")
-  system(paste("yo syberia:model", name))
-}
+syberia_new_model <- function(name) syberia_subgenerator(name, type = 'model')
+
 
 #' Bootstrap a new classifier for a syberia project
 #'
-#' @param name character. The name of the classifierb
+#' @param name character. The name of the classifier.
 #' @export
-syberia_new_classifier <- function(name) {
-  stopifnot(is.character(name) && length(name) == 1)
-  if (is.null(syberia_root())) stop("You need to create a syberia project first. Check if you are in the right directory.")
-  system(paste("yo syberia:classifier", name))
-}
+syberia_new_classifier <- function(name) syberia_subgenerator(name, type = 'classifier')
+
+#' Bootstrap a new mungebit for a syberia project
+#'
+#' @param name character. The name of the mungebit.
+#' @export
+syberia_new_mungebit <- function(name) syberia_subgenerator(name, type = 'mungebit')
