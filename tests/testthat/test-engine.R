16df5a1563ae45490f4351561ae5df95850b2d88
RobertZK <technoguyrob@gmail.com>
2015-07-20 15:52:19 -0500

    Check idempotent and non-idempotent versions of application.R when using syberia_engine.

--- tests/testthat/test-engine.R
+++ tests/testthat/test-engine.R
@@ -13,5 +13,23 @@ describe("Engine object", {
       expect_false(is.null(syberia_engine(tempdir)))
     })
   })
+
+  test_that("it can find an engine with an application.R file", {
+    testthatsomemore::within_file_structure(list("config" = list("application.R")), {
+      expect_false(is.null(syberia_engine(tempdir)))
+    })
+  })
+
+  test_that("it can find an engine with an application/application.R file", {
+    testthatsomemore::within_file_structure(list("config" = list("application" = list("application.R"))), {
+      expect_false(is.null(syberia_engine(tempdir)))
+    })
+  })
+
+  test_that("it can find an engine with an application/application.r file", {
+    testthatsomemore::within_file_structure(list("config" = list("application" = list("application.r"))), {
+      expect_false(is.null(syberia_engine(tempdir)))
+    })
+  })
 })
 
