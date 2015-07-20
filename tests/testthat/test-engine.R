ab58f26c4393512cb4dfb770dc102c0afc5cb795
RobertZK <technoguyrob@gmail.com>
2015-07-20 15:51:21 -0500

    Test that syberia_engine finds an engine.

--- tests/testthat/test-engine.R
+++ tests/testthat/test-engine.R
@@ -1,8 +1,17 @@
 context("engine")
+library(testthatsomemore)
 
 describe("Invalid inputs", {
   test_that("it errors when an non-engine directory is passed", {
     expect_error(syberia_engine(tempfile()), "No syberia engine")
   })
-
 })
+
+describe("Engine object", {
+  test_that("it can find an engine with an application.r file", {
+    testthatsomemore::within_file_structure(list("config" = list("application.r")), {
+      expect_false(is.null(syberia_engine(tempdir)))
+    })
+  })
+})
+
