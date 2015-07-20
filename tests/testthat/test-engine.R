988b54f36ba993deaf4b75f4f39e93366a669d4d
RobertZK <technoguyrob@gmail.com>
2015-07-20 15:54:13 -0500

    test that it can find an engine from a nested directory

--- tests/testthat/test-engine.R
+++ tests/testthat/test-engine.R
@@ -5,9 +5,15 @@ describe("Invalid inputs", {
   test_that("it errors when an non-engine directory is passed", {
     expect_error(syberia_engine(tempfile()), "No syberia engine")
   })
+
+  test_that("it can't find the engine if application.R is misspelled", {
+    testthatsomemore::within_file_structure(list("config" = list("applicashun.r")), {
+    expect_error(syberia_engine(tempdir), "No syberia engine")
+    })
+  })
 })
 
-describe("Engine object", {
+describe("Finding the engine", {
   test_that("it can find an engine with an application.r file", {
     testthatsomemore::within_file_structure(list("config" = list("application.r")), {
       expect_false(is.null(syberia_engine(tempdir)))
@@ -29,6 +35,12 @@ describe("Engine object", {
   test_that("it can find an engine with an application/application.r file", {
     testthatsomemore::within_file_structure(list("config" = list("application" = list("application.r"))), {
       expect_false(is.null(syberia_engine(tempdir)))
+    })
+  })
+
+  test_that("it can find an engine from a nested directory", {
+    testthatsomemore::within_file_structure(list("foo" = list("bar.R"), "config" = list("application.R")), {
+      expect_false(is.null(syberia_engine(file.path(tempdir, "foo", "bar.R"))))
     })
   })
 })
