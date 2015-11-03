a29cb2e6682b0ac1d141155615661b500cf4337e
RobertZK <technoguyrob@gmail.com>
2015-11-03 00:07:36 -0600

    Remove unnecessary comment on nested controller engine

--- tests/testthat/projects/nested_controller/example/config/engines.R
+++ tests/testthat/projects/nested_controller/example/config/engines.R
@@ -3,14 +3,7 @@ engine("base", type = "local",
        mount = TRUE)
 
 .onAttach <- function(parent_engine) {
-  # Fetch the routes from *this* engine.
-  #routes <- director$resource("config/routes", parent. = FALSE, parse. = FALSE)
-
-  #routes_controller <- director$resource("lib/controllers/routes")$parser
-  #routes_controller$parser(director = parent_engine, 
-  #  output = routes, any_dependencies_modified = FALSE,
-  #  args = list(force = TRUE))
-    # `director` is the base engine object.
+  # Mount the routes on the parent engine.
   routes <- director$.engines$base$engine$resource("lib/controllers/routes", parent. = FALSE)
 
   parent_engine$register_parser("config/routes", routes$parser, overwrite = TRUE)
