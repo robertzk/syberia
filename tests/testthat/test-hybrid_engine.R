827d23cdbbc3221e40dc095ba00d5d27d87f9fe5
RobertZK <technoguyrob@gmail.com>
2015-11-13 08:31:23 -0600

    TODO for hybrid engines.

---
+++ tests/testthat/test-hybrid_engine.R
@@ -0,0 +1,6 @@
+context("hybrid engines")
+
+# TODO: (RK) Test that a DAG of mounted and utility engines co-operatr
+# in harmony, even when some of the mounted engines share common utility
+# engines.
+
