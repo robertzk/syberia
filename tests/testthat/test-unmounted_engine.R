cfbb069ced8c4b965aedfd9babf24fcdb46e8d83
RobertZK <technoguyrob@gmail.com>
2015-11-13 07:27:46 -0600

    Initial failing utility engine test.

---
+++ tests/testthat/test-unmounted_engine.R
@@ -0,0 +1,13 @@
+context("unmounted engine")
+
+describe("A simple non-mounted utility engine", {
+  test_that("it can load a resource from another non-mounted engine", {
+    expect_equal(syberia_engine("projects/utility_engines/main")$resource("hello"),
+                 "hello world")
+  })
+
+  test_that("it cannot load a resource from another non-mounted engine without direct specification", {
+    expect_error(syberia_engine("projects/utility_engines/main")$resource("hello_raw"), "Cannot")
+  })
+})
+
