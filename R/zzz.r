5164cd9ff967dbf04781c968b43266dc4cf9d50c
RobertZK <technoguyrob@gmail.com>
2015-11-16 14:46:24 -0600

    Clean up onAttach hook

--- R/zzz.r
+++ R/zzz.r
@@ -6,10 +6,15 @@
   # We want to initialize a Syberia project in the current working directory
   # because 9 times out of 10 this is what the user wants.
   #
-  # However, this hook doesn"t work on install, because install is done from
+  # However, this hook doesn't work on install, because install is done from
   # the working directory of the package, not the user.
   #
   # But when the user calls library(syberia), this will work.
-  try(syberia_project(), silent = TRUE)
+  try({
+    syberia_engine()
+    packageStartupMessage(crayon::yellow(
+      "Loaded syberia project ", sQuote(active_project()$root()), "...\n"
+    ))
+  }, silent = TRUE)
 }
 
