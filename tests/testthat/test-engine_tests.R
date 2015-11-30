3b6e9b13154f55b87c273f23d7368ca8bdd4e6cd
RobertZK <technoguyrob@gmail.com>
2015-11-29 20:29:32 -0600

    Wrap with test_that block.

--- tests/testthat/test-engine_tests.R
+++ tests/testthat/test-engine_tests.R
@@ -5,7 +5,9 @@ bag_of_objects <- list(force, 1, NULL, "foo", list(), new.env())
 
 describe("invalid inputs", {
   lapply(Filter(Negate(is.character), bag_of_objects), function(object) {
-    expect_error(test_engine(object), "Please pass a")
+    test_that(paste("it fails when test_engine is called with a", class(object)), {
+      expect_error(test_engine(object), "Please pass a")
+    })
   })
 })
 
