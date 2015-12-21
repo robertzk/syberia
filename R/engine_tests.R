f4e885ae96e6762855698442b69793e82d7d1db3
RobertZK <technoguyrob@gmail.com>
2015-12-21 12:59:44 -0600

    Document new test_engine parameters.

--- R/engine_tests.R
+++ R/engine_tests.R
@@ -22,6 +22,14 @@
 #'    If a \code{character}, it will be passed to \code{\link{syberia_engine}} first.
 #' @param base character. Any subdirectory to test specifically. By default,
 #'    \code{"test"}.
+#' @param config character. The relative path to the configuration resource,
+#'    by default \code{"config/environments/test"}.
+#' @param ignored_tests character. The list of tests to ignore, by default
+#'    the local variable \code{ignored_tests} extracted from the configuration
+#'    resource specific by the \code{config} parameter.
+#' @param required logical. Whether or not all tests are required to have resources,
+#'    by default \code{TRUE}. If \code{TRUE}, the \code{ignored_tests}
+#'    resources will not be required to have an accompanying test.
 #' @seealso \code{\link{syberia_engine}}
 #' @export
 #' @return \code{TRUE} if all tests pass or will error otherwise. Note this
@@ -29,7 +37,8 @@
 #'    represent progress if available.
 test_engine <- function(engine = syberia_engine(), base = "test",
                         config = file.path("config", "environments", "test"),
-                        ignored_tests = ignored_tests_from_config(engine, base, config)) {
+                        ignored_tests = ignored_tests_from_config(engine, base, config),
+                        required = TRUE) {
   if (is.character(engine)) {
     engine <- syberia_engine(engine)
   }
@@ -40,6 +49,8 @@ test_engine <- function(engine = syberia_engine(), base = "test",
 
   force(ignored_tests)
   tests <- find_tests(engine, base, ignored_tests)
+
+  ensure_resources_have_tests
 
   # TODO: (RK) Actually run the tests.
 }
@@ -55,7 +66,7 @@ find_tests <- function(engine, base, ignored_tests) {
   )
 }
 
-test_environment_config <- function(engine, path = file.path("config", "environments", "test")) {
+test_environment_configuration <- function(engine, path = file.path("config", "environments", "test")) {
   if (!engine$exists(path, children. = FALSE)) {
     list()
   } else {
@@ -65,7 +76,7 @@ test_environment_config <- function(engine, path = file.path("config", "environm
 
 ignored_tests_from_config <- function(engine, base, config) {
   file.path(base,
-    (test_environment_config(engine, config)$ignored_tests) %||% character(0)
+    (test_environment_configuration(engine, config)$ignored_tests) %||% character(0)
   )
 }
 
