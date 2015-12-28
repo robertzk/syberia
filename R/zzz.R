8b25c032b0cc007fe4b2dadcdf6cf1501bff486c
RobertZK <technoguyrob@gmail.com>
2015-12-28 16:18:14 -0600

    Set CI=TRUE.

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,36 @@
 .onAttach <- function(...) {
+  if (!isTRUE(getOption("syberia.silent"))) {
+    packageStartupMessage(paste0("Loading ", crayon::red("Syberia"), "...\n"))
+  }
+
+  # Load better trace.
+  if (isTRUE(getOption("syberia.autoload_bettertrace", TRUE)) &&
+      !identical(Sys.getenv("CI"), "TRUE")) {
+    if (!is.element("devtools", utils::installed.packages()[, 1])) {
+      packageStartupMessage(crayon::yellow("   ...Installing devtools\n"))
+      utils::install.packages("devtools")
+    }
+
+    if (!requireNamespace("bettertrace", quietly = TRUE)) {
+      packageStartupMessage(crayon::yellow("   ...Installing github.com/robertzk/bettertrace\n"))
+      devtools::install_github("robertzk/bettertrace")
+    }
+    library(bettertrace)
+  }
+  
+  # We want to initialize a Syberia project in the current working directory
+  # because 9 times out of 10 this is what the user wants.
+  #
+  # However, this hook doesn't work on install, because install is done from
+  # the working directory of the package, not the user.
+  #
+  # But when the user calls library(syberia), this will work.
+  try({
+    syberia_engine()
+    packageStartupMessage(crayon::yellow(
+      "Loaded syberia project ", sQuote(active_project()$root()), "...\n"
+    ))
+  }, silent = TRUE)
 }
 
 .onDetach <- function(...) {
