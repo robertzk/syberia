7b9b19b6ac976cd935601e00d9427991fc4a5721
RobertZK <technoguyrob@gmail.com>
2015-12-31 20:57:45 -0600

    Replace project activeBinding with one in the global environment.

--- R/zzz.R
+++ R/zzz.R
@@ -1,10 +1,11 @@
-install_options <- function() {
-  # Use Rstudio as the default CRAN mirror.
-  c(repos = getOption("repos", structure(c(CRAN = "http://cran.rstudio.com"))))
-}
+.onAttach <- function(...) {
+  if (!isTRUE(getOption("syberia.silent"))) {
+    packageStartupMessage(paste0("Loading ", crayon::red("Syberia"), "...\n"))
+  }
 
-install_bettertrace <- function() {
-  devtools::with_options(install_options(), {
+  # Load better trace.
+  if (isTRUE(getOption("syberia.autoload_bettertrace", TRUE)) &&
+      !identical(Sys.getenv("CI"), "TRUE")) {
     if (!is.element("devtools", utils::installed.packages()[, 1])) {
       packageStartupMessage(crayon::yellow("   ...Installing devtools\n"))
       utils::install.packages("devtools")
@@ -15,20 +16,13 @@ install_bettertrace <- function() {
       devtools::install_github("robertzk/bettertrace")
     }
     library(bettertrace)
-  })
-}
-
-.onAttach <- function(...) {
-  if (!isTRUE(getOption("syberia.silent"))) {
-    packageStartupMessage(paste0("Loading ", crayon::red("Syberia"), "...\n"))
   }
 
-  # Load bettertrace.
-  if (isTRUE(getOption("syberia.autoload_bettertrace", TRUE)) &&
-      !identical(Sys.getenv("CI"), "TRUE")) {
-    install_bettertrace()
-  }
-
+  makeActiveBinding("project", env = globalenv(),
+    function() getFromNamespace("active_project", "syberia")())
+  makeActiveBinding("resource", env = globalenv(),
+    function() getFromNamespace("active_project", "syberia")()$resource)
+  
   # We want to initialize a Syberia project in the current working directory
   # because 9 times out of 10 this is what the user wants.
   #
@@ -45,6 +39,12 @@ install_bettertrace <- function() {
 }
 
 .onDetach <- function(...) {
+  try(silent = TRUE, detach("syberia:shims"))
   # TODO: (RK) Detach any environments attached to the search path
   # by the active project.
 }
+
+.onUnload <- function(...) {
+  try(silent = TRUE, detach("syberia:shims"))
+}
+
