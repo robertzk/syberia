4d6cd592273c7a7b3248a29752f82f587f334545
Kirill Sevastyanenko <kirillseva@gmail.com>
2016-01-14 12:12:54 -0600

    set default CRAN repo for installation

--- R/zzz.R
+++ R/zzz.R
@@ -3,21 +3,25 @@
     packageStartupMessage(paste0("Loading ", crayon::red("Syberia"), "...\n"))
   }
 
-  # Load better trace.
-  if (isTRUE(getOption("syberia.autoload_bettertrace", TRUE)) &&
-      !identical(Sys.getenv("CI"), "TRUE")) {
-    if (!is.element("devtools", utils::installed.packages()[, 1])) {
-      packageStartupMessage(crayon::yellow("   ...Installing devtools\n"))
-      utils::install.packages("devtools")
-    }
+  # Load bettertrace.
+  devtools::with_options(
+    c(repos=structure(c(CRAN="http://streaming.stat.iastate.edu/CRAN/"))),
+    {
+      if (isTRUE(getOption("syberia.autoload_bettertrace", TRUE)) &&
+          !identical(Sys.getenv("CI"), "TRUE")) {
+        if (!is.element("devtools", utils::installed.packages()[, 1])) {
+          packageStartupMessage(crayon::yellow("   ...Installing devtools\n"))
+          utils::install.packages("devtools")
+        }
 
-    if (!requireNamespace("bettertrace", quietly = TRUE)) {
-      packageStartupMessage(crayon::yellow("   ...Installing github.com/robertzk/bettertrace\n"))
-      devtools::install_github("robertzk/bettertrace")
+        if (!requireNamespace("bettertrace", quietly = TRUE)) {
+          packageStartupMessage(crayon::yellow("   ...Installing github.com/robertzk/bettertrace\n"))
+          devtools::install_github("robertzk/bettertrace")
+        }
+        library(bettertrace)
+      }
     }
-    library(bettertrace)
-  }
-  
+  )
   # We want to initialize a Syberia project in the current working directory
   # because 9 times out of 10 this is what the user wants.
   #
@@ -37,4 +41,3 @@
   # TODO: (RK) Detach any environments attached to the search path
   # by the active project.
 }
-
