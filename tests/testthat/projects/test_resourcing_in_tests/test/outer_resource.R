9b7f36b5e56731698f1505c5d207a4ced79657cd
abelcastilloavant <abel.castillo@avant.com>
2016-11-18 15:26:59 -0600

    don't start resource names with test

--- tests/testthat/projects/test_resourcing_in_tests/test/outer_resource.R
+++ tests/testthat/projects/test_resourcing_in_tests/test/outer_resource.R
@@ -3,5 +3,5 @@ test_that("A call to `resource` with no parameters sources the tested resource",
 })
 
 test_that("A call to `resource` with a parameter sources the named resource", {
-  expect_equal(resource("resource_me"), "This resource got called in another resource's test")
+  expect_equal(resource("inner_resource"), "This resource got called in another resource's test")
 })
